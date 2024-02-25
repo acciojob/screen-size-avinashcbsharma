b0b653ab08cf4ca4a7ace06788ff73c00e43e3a7
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-25 06:37:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+function changeSize(){
+  // window.open("","","width=665px,height=359px,left=1oopx,top=100px");
+  var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  document.getElementById('txt').textContent = 'Width: ' + width + ' Height: ' + height;
+}
+changeSize()
+window.addEventListener('resize',changeSize);
